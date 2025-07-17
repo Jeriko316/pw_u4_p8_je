9a36807b18b61f98e61a044f04c38cad04730990
Jeriko316 <jkenriquez@hotmail.com>
2025-07-16 19:55:03 -0500

    taller37

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EstudianteView from '@/views/EstudianteView'
+import LoginView from '@/views/LoginView.vue'
+import HomeView from '@/views/HomeView.vue'
+
+function estaAutenticado(){
+  let result =  localStorage.getItem('auth') === 'true'
+  console.log(result);
+  return result;
+}
 
 const routes = [
   {
     path: '/estudiante',
     name: 'estudiante',
     component: EstudianteView
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: HomeView,
+    meta: {
+      requiereAutenticacion: true //protegia
+    }
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to,from,next) =>{
+  console.log('antes')
+  if(to.meta.requiereAutenticacion){
+     console.log('auth')
+    //Si no esta autenticado
+    if(!estaAutenticado()){
+       console.log('exito')
+      next('/login')
+    }else{
+      next();
+    }
+  }else{
+    next();
+  }
 })
 
 export default router
